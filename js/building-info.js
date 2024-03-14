ec94789bd9e584b977c8f5eae714dd6d93d809d2
meobeoo <hoaphamduc2399@gmail.com>
2024-03-14 16:50:39 +0700

    fix text

--- js/building-info.js
+++ js/building-info.js
@@ -1184,8 +1184,8 @@ function createInfoDiv(buildingKey) {
   toolbar.className = 'toolbar';
   toolbar.innerHTML = `
       <button class="direct-btn" onclick="initRoutingControl('${buildingKey}')"></button>
-      <span class="tim-duong contentVN">Chỉ đường : </span>
-      <span class="tim-duong contentEnglish">Directions : </span>
+      <span class="tim-duong contentVN">Chỉ đường (có thể mất vài phút) : </span>
+      <span class="tim-duong contentEnglish">Directions (take a few minutes) : </span>
       <button class="direct-btn-ggm" onclick="openGoogleMaps('${buildingKey}')"></button>
       <span class="tim-duong-ggm contentVN">Chỉ đường bằng Google Maps: </span>
       <span class="tim-duong-ggm contentEnglish">Directions using Google Maps: </span>
